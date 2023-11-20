89295247c285efe64d5956b902cf23fd962e4366
Lương Nguyễn Khoa <107914230+CRLLNKhoa@users.noreply.github.com>
2023-11-20 10:55:29 +0700

    Update data.js

--- data.js
+++ data.js
@@ -13,6 +13,18 @@ export const project = [
     linkGithub: "https://github.com/CRLLNKhoa/linktree",
     type: [{ title: "Tailwind", value: "tailwind" },{title: "React.js",value: "react.js"}],
     img: "./linktree.png",
-    description: "In this tutorial, we will use the Next.js 13 App Router to build a fully functional E-Commerce with Admin Dashboard with seamless navigation and user experience."
+    description: "The Link Tree project that I coded is an application that brings convenience and flexibility to sharing multiple links from a single destination page. Through a simple and user-friendly interface, users can create a centralized hub for their important links, such as personal websites, social media profiles, products, and services.
+
+What's particularly noteworthy is the high level of customization in my Link Tree project, allowing users to tailor the appearance to their personal style or brand, from background images to colors and descriptions for each link. Additionally, the tracking and analytics features provide users with insights into the performance of the shared links.
+
+I hope that this project will help users optimize their experience on social media, providing a convenient and friendly approach to introducing various content and connecting with their online community."
+  },
+    {
+    name: "Form Builder",
+    slug: "form-builder",
+    linkGithub: "https://github.com/CRLLNKhoa/nextjs13_form_builder",
+    type: [{ title: "Tailwind", value: "tailwind" },{title: "Next.js",value: "next.js"}],
+    img: "https://user-images.githubusercontent.com/107914230/284124542-f30cd9af-0944-438a-b17a-37e747d7e545.png",
+    description: "A form builder is a tool or software application designed to create and customize digital forms for various purposes, such as surveys, questionnaires, registrations, and more."
   },
 ];
