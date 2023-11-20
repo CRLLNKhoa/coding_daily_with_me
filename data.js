dfb3e19a1c67e8ccd5624934f1ea244fea30ca9d
Lương Nguyễn Khoa <107914230+CRLLNKhoa@users.noreply.github.com>
2023-11-20 11:11:18 +0700

    Update data.js

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ export const project = [
     linkGithub: "https://github.com/CRLLNKhoa/time_logs_project",
     type: [{ title: "Tailwind", value: "tailwind" },{title: "React.js",value: "react.js"}],
     img: "./2023.png",
-    description: "In this tutorial, we will use the Next.js 13 App Router to build a fully functional E-Commerce with Admin Dashboard with seamless navigation and user experience."
+    description: "The Time Log project that I developed is an application designed to assist users in tracking and efficiently managing their time. With a simple and user-friendly interface, users have the capability to register and record their daily activities, ranging from work and projects to significant events."
   },
    {
     name: "Links tree app",
@@ -13,11 +13,7 @@ export const project = [
     linkGithub: "https://github.com/CRLLNKhoa/linktree",
     type: [{ title: "Tailwind", value: "tailwind" },{title: "React.js",value: "react.js"}],
     img: "./linktree.png",
-    description: "The Link Tree project that I coded is an application that brings convenience and flexibility to sharing multiple links from a single destination page. Through a simple and user-friendly interface, users can create a centralized hub for their important links, such as personal websites, social media profiles, products, and services.
-
-What's particularly noteworthy is the high level of customization in my Link Tree project, allowing users to tailor the appearance to their personal style or brand, from background images to colors and descriptions for each link. Additionally, the tracking and analytics features provide users with insights into the performance of the shared links.
-
-I hope that this project will help users optimize their experience on social media, providing a convenient and friendly approach to introducing various content and connecting with their online community."
+    description: "The Link Tree project that I coded is an application that brings convenience and flexibility to sharing multiple links from a single destination page. Through a simple and user-friendly interface, users can create a centralized hub for their important links, such as personal websites, social media profiles, products, and services. Whats particularly noteworthy is the high level of customization in my Link Tree project, allowing users to tailor the appearance to their personal style or brand, from background images to colors and descriptions for each link. Additionally, the tracking and analytics features provide users with insights into the performance of the shared links. I hope that this project will help users optimize their experience on social media, providing a convenient and friendly approach to introducing various content and connecting with their online community."
   },
     {
     name: "Form Builder",
